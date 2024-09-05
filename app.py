10ea0cd8ba02d32b42f0da5c53420c74e66a6015
anh-ava <anh.a.phan@avanade.com>
2024-09-05 11:45:03 +0930

    Update 1 app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+   name = request.form.get('name') + 'test'
 
    if name:
        print('Request for hello page received with name=%s' % name)
